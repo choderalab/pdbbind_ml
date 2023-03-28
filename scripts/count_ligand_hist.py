3edd43c9e702d91fef7e5e89c703618a52e46470
kaminow <benjamin.kaminow@choderalab.org>
2023-03-28 11:37:00 -0400

    Script to calculate ligand distribution.

---
+++ scripts/count_ligand_hist.py
@@ -0,0 +1,52 @@
+import argparse
+import matplotlib.pyplot as plt
+import pandas
+import seaborn as sns
+
+
+################################################################################
+def get_args():
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument("-i", "--in_file", required=True, help="Input CSV file.")
+    parser.add_argument("-o", "--out_file", required=True, help="Output base filename.")
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    # Load input
+    df = pandas.read_csv(args.in_file, index_col=0)
+
+    # Count occurrences of each ligand id
+    lig_counts = df["ligand_id"].value_counts().to_frame().reset_index()
+    lig_counts = lig_counts.rename(columns={"index": "ligand_id", "ligand_id": "count"})
+
+    # Count occurrences of each count
+    count_counts = lig_counts["count"].value_counts().to_frame().reset_index()
+    count_counts = count_counts.rename(
+        columns={"index": "occurrences", "count": "n_ligs"}
+    )
+
+    # # Get order for x axis
+    # x_order = map(str, sorted(lig_counts["count"].values))
+
+    # # Set to str so bins aren't numerical
+    # lig_counts.loc[:, "count"] = lig_counts["count"].astype(str)
+
+    # Plot
+    fig, ax = plt.subplots(figsize=(12, 8))
+    sns.barplot(count_counts, x="occurrences", y="n_ligs")
+    # sns.histplot(x=map(str, sorted(lig_counts["count"].values)))
+    ax.set_xlabel("Ligand Occurrences")
+    ax.set_ylabel("Number of Ligands")
+    ax.set_title("Ligand Distribution in PDBBind")
+    fig.savefig(f"{args.out_file}.png", bbox_inches="tight", dpi=200)
+
+    count_counts.to_csv(f"{args.out_file}.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
