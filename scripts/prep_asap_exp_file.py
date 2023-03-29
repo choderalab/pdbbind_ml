d03dab619d9236dd15e7a5dbae538f74cd186ac8
kaminow <benjamin.kaminow@choderalab.org>
2023-03-29 15:26:51 -0400

    Script to prep PDBBind experimental information to be ready for the asapdiscovery pipeline.

---
+++ scripts/prep_asap_exp_file.py
@@ -0,0 +1,89 @@
+"""
+Script to prepare the output of parse_pdbbind_affinities.py for ingestion by the
+asapdiscovery pipeline (namely, asapdiscovery.data.utils.cdd_to_schema).
+"""
+
+import argparse
+import numpy as np
+import pandas
+
+
+def convert_row(r):
+    """
+    Convert a row of the input CSV file into a row of output format.
+
+    Parameters
+    ----------
+    r : pandas.Series
+        Row of input DataFrame
+
+    Returns
+    -------
+    pandas.Series
+        Row of output DataFrame
+    """
+
+    # Needed output columns:
+    #  * suspected_SMILES
+    #  * Canonical PostEra ID
+    #  * ProteaseAssay_Fluorescence_Dose-Response_Weizmann: Avg pIC50
+    #  * ProteaseAssay_Fluorescence_Dose-Response_Weizmann: IC50 CI (Lower) (µM)
+    #  * ProteaseAssay_Fluorescence_Dose-Response_Weizmann: IC50 CI (Upper) (µM)
+
+    smi = r["iso_smiles"] if "iso_smiles" in r.index else r["str_smiles"]
+    ligand_id = r["ligand_id"]
+    # For now, only handle IC50 measurements
+    # Ki/Kd conversion will need to use Cheng-Prussof
+    pic50 = -np.log10(r["measurement_value"])
+    # pic50 = convert_to_pic50(r["measurement_value"], r["measurement_type"])
+
+    return pandas.Series(
+        [smi, ligand_id, pic50, np.nan, np.nan],
+        index=[
+            "suspected_SMILES",
+            "Canonical PostEra ID",
+            "ProteaseAssay_Fluorescence_Dose-Response_Weizmann: Avg pIC50",
+            "ProteaseAssay_Fluorescence_Dose-Response_Weizmann: IC50 CI (Lower) (µM)",
+            "ProteaseAssay_Fluorescence_Dose-Response_Weizmann: IC50 CI (Upper) (µM)",
+        ],
+    )
+
+
+def convert_to_pic50(measurement_value, measurement_type):
+    """
+    Convert an experimental measurement to a pIC50 value.
+
+    Parameters
+    ----------
+    measurement_value : float
+        Measurement value
+    measurement_type : str
+        What type of measurement the input is. Should be one of ["Ki", "IC50", "Kd"]
+
+    Returns
+    -------
+    float
+        Measurement converted to pIC50 value
+    """
+    pass
+
+
+################################################################################
+def get_args():
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument("-i", "--in_file", required=True, help="Input CSV file.")
+    parser.add_argument("-o", "--out_file", required=True, help="Input CSV file.")
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    df_in = pandas.read_csv(args.in_file, index_col=0)
+    df_out = df_in.apply(convert_row, axis=1)
+    df_out.to_csv(args.out_file, index=False)
+
+if __name__ == '__main__':
+    main()
